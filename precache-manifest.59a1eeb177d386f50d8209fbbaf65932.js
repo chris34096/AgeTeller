bb52b7dbeaa944874a6b9709383e5afcdb5b0163
Christofer Perez <christoferperez@gmail.com>
2019-09-16 22:25:19 +0200

    Updates

---
+++ precache-manifest.59a1eeb177d386f50d8209fbbaf65932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c202606e4c044b44535e73b321061f",
+    "url": "/chris34096/Personal-Project-Age-Teller-React-Redux-/index.html"
+  },
+  {
+    "revision": "46f32d89bdd1e96d3e96",
+    "url": "/chris34096/Personal-Project-Age-Teller-React-Redux-/static/css/main.287d2604.chunk.css"
+  },
+  {
+    "revision": "1fce5e02b25b5eeaeaba",
+    "url": "/chris34096/Personal-Project-Age-Teller-React-Redux-/static/js/2.6f54c7d0.chunk.js"
+  },
+  {
+    "revision": "46f32d89bdd1e96d3e96",
+    "url": "/chris34096/Personal-Project-Age-Teller-React-Redux-/static/js/main.5b455671.chunk.js"
+  },
+  {
+    "revision": "e7e554146c35d3119b5f",
+    "url": "/chris34096/Personal-Project-Age-Teller-React-Redux-/static/js/runtime~main.2cd76ba0.js"
+  },
+  {
+    "revision": "2a8c16ba3bac31f0e39648de78e14406",
+    "url": "/chris34096/Personal-Project-Age-Teller-React-Redux-/static/media/congratulation.2a8c16ba.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/chris34096/Personal-Project-Age-Teller-React-Redux-/precache-manifest.59a1eeb177d386f50d8209fbbaf65932.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chris34096/Personal-Project-Age-Teller-React-Redux-/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
